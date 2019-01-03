2b4b8a4f567a56781b19c8b2c10d71613f596a07
YaoTiancheng <yaotiancheng@outlook.com>
2019-01-03 21:51:52 +0800

    Seperated part of the RayTracing.hlsl into multiple files.

--- RayTracing.hlsl
+++ RayTracing.hlsl
@@ -32,132 +32,16 @@ struct RayTracingConstants
 StructuredBuffer<Sphere>                g_Spheres       : register(t0);
 StructuredBuffer<PointLight>            g_PointLights   : register(t1);
 StructuredBuffer<RayTracingConstants>   g_Constants     : register(t2);
-StructuredBuffer<float>                 g_Samples       : register(t3);
 RWTexture2D<float4>                     g_FilmTexture;
 
-#define PI 3.1415
+static const float PI       = 3.14159265359;
+static const float INV_PI   = 1 / PI;
 
-groupshared uint gs_NextSampleIndex = 0;
+#include "Samples.hlsl.inc"
+#include "MonteCarlo.hlsl.inc"
+#include "BSDFs.hlsl.inc"
+#include "Primitives.hlsl.inc"
 
-float GetNextSample()
-{
-    uint sampleIndex;
-    InterlockedAdd(gs_NextSampleIndex, 1, sampleIndex);
-    sampleIndex = sampleIndex % g_Constants[0].samplesCount;
-    return g_Samples[sampleIndex];
-}
-
-float2 GetNextSample2()
-{
-    return float2(GetNextSample(), GetNextSample());
-}
-
-float2 ConcentricSampleDisk(float2 sample)
-{
-    float r, theta;
-    float2 s = 2 * sample - 1;
-
-    if (s.x == 0.0f && s.y == 0.0f)
-    {
-        return (float2) 0.0f;
-    }
-    if (s.x >= -s.y)
-    {
-        if (s.x > s.y)
-        {
-            r = s.x;
-            if (s.y > 0.0f)
-                theta = s.y / r;
-            else
-                theta = 8.0f + s.y / r;
-        }
-        else
-        {
-            r = s.y;
-            theta = 2.0f - s.x / r;
-        }
-    }
-    else
-    {
-        if (s.x <= s.y)
-        {
-            r = -s.x;
-            theta = 4.0f - s.y / r;
-        }
-        else
-        {
-            r = -s.y;
-            theta = 6.0f + s.x / r;
-        }
-    }
-
-    theta *= PI / 4.0f;
-    return r * float2(cos(theta), sin(theta));
-}
-
-float4 ConsineSampleHemisphere(float2 sample)
-{
-    sample = ConcentricSampleDisk(sample);
-    return float4(sample.xy, sqrt(max(0.0f, 1.0f - dot(sample.xy, sample.xy))), 0.0f);
-}
-
-bool RaySphereIntersect(float4 origin
-    , float4 direction
-    , Sphere sphere
-    , out float t)
-{
-    float radius2 = sphere.radius * sphere.radius;
-    float t0, t1;
-    float4 l = sphere.position - origin;
-    float tca = dot(l, direction);
-    if (tca < 0.0f)
-        return false;
-    float d2 = dot(l, l) - tca * tca;
-    if (d2 > radius2)
-        return false;
-    float thc = sqrt(radius2 - d2);
-    t0 = tca - thc;
-    t1 = tca + thc;
-
-    if (t0 > t1)
-    {
-        float temp = t0;
-        t0 = t1;
-        t1 = temp;
-    }
-    if (t0 < 0)
-    {
-        t0 = t1;
-        if (t0 < 0)
-            return false;
-    }
-    t = t0;
-    return true;
-}
-
-bool RaySphereIntersect(float4 origin
-    , float4 direction
-    , Sphere sphere
-    , out float t
-    , out float4 position
-    , out float4 normal
-    , out float4 tangent
-    , out float4 albedo
-    , out float4 emission)
-{
-    bool intersect = false;
-    if (intersect = RaySphereIntersect(origin, direction, sphere, t))
-    {
-        position = origin + t * direction;
-        normal = normalize(position - sphere.position);
-        tangent = normalize(float4(cross(float3(0.0f, 1.0f, 0.0f), normal.xyz), 0.0f));
-        if (isinf(tangent.x))
-            tangent = float4(1.0f, 0.0f, 0.0f, 0.0f);
-        albedo = sphere.albedo;
-        emission = sphere.emission;
-    }
-    return intersect;
-}
 
 void GenerateRay(float2 sample
 	, float2 filmSize
@@ -173,33 +57,6 @@ void GenerateRay(float2 sample
 
     origin = mul(origin, cameraTransform);
     direction = mul(direction, cameraTransform);
-}
-
-void SampleLambertBRDF(float4 wo
-	, float2 sample
-    , float4 albedo
-    , float4 normal
-    , float4 tangent
-    , out float4 wi
-    , out float4 value
-    , out float4 pdf)
-{
-    float4 biNormal = float4(cross(tangent.xyz, normal.xyz), 0.0f);
-    float4x4 tbn2world = float4x4(tangent, biNormal, normal, float4(0.0f, 0.0f, 0.0f, 1.0f));
-    float4x4 world2tbn = transpose(tbn2world);
-
-    wo = mul(wo, world2tbn);
-
-    wi = ConsineSampleHemisphere(sample);
-    value = wi.z > 0.0f ? albedo / PI : 0.0f;
-    pdf = dot(float4(0.0f, 0.0f, 1.0f, 0.0f), wi) / PI;
-
-    wi = mul(wi, tbn2world);
-}
-
-float4 EvaluateLambertBRDF(float4 wo, float4 wi, float4 albedo)
-{
-    return albedo / PI;
 }
 
 bool IntersectScene(float4 origin

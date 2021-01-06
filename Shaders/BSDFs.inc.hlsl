fc2392c3e24157492127d2beb60b312e3bb2d66c
Yao Tian Cheng <tian-cheng.yao@ubisoft.com>
2021-01-06 17:21:36 +0800

    * Replaces wo.z with WOdotN from the lighting context.

--- Shaders/BSDFs.inc.hlsl
+++ Shaders/BSDFs.inc.hlsl
@@ -52,9 +52,9 @@ float3 EvaluateBSDF( float3 wi, float3 wo, Intersection intersection )
     float3 value = EvaluateCookTorranceMircofacetBRDF( wi, wo, intersection.specular, intersection.alpha, 1.0f, intersection.ior, lightingContext );
     value += EvaluateCookTorranceCompBRDF( wi, wo, intersection.specular, intersection.alpha, intersection.ior, lightingContext );
 
-    float E = EvaluateCookTorranceCompE( wo.z, intersection.alpha );
+    float E = EvaluateCookTorranceCompE( lightingContext.WOdotN, intersection.alpha );
     float EAvg = EvaluateCookTorranceCompEAvg( intersection.alpha );
-    float cosThetaO = wo.z;
+    float cosThetaO = lightingContext.WOdotN;
     float specularWeight        = SpecularWeight( cosThetaO, intersection.alpha, intersection.ior );
     float specularCompWeight    = SpecularCompWeight( intersection.ior, E, EAvg );
     float diffuseWeight = 1.0f - specularWeight - specularCompWeight;
@@ -79,11 +79,11 @@ void SampleBSDF( float3 wo
 
     LightingContext lightingContext = InitLightingContext( wo );
 
-    bool invert = wo.z < 0.0f;
+    bool invert = lightingContext.WOdotN < 0.0f;
     float etaI = invert ? intersection.ior : 1.0f;
     float etaT = invert ? 1.0f : intersection.ior;
 
-    float cosThetaO = wo.z;
+    float cosThetaO = lightingContext.WOdotN;
     float E = EvaluateCookTorranceCompE( cosThetaO, intersection.alpha );
     float EAvg = EvaluateCookTorranceCompEAvg( intersection.alpha );
     float specularWeight = SpecularWeight( cosThetaO, intersection.alpha, intersection.ior );

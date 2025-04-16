5a1826ebf92aebff264950d9a75ad2b2c4fd721f
yaotiancheng <yaotiancheng@outlook.com>
2025-04-16 12:26:42 +0800

    Enable CRT heap leak check in debug build.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -80,6 +80,8 @@ int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
     _In_ LPWSTR    lpCmdLine,
     _In_ int       nCmdShow )
 {
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+
     UNREFERENCED_PARAMETER( hPrevInstance );
 
     CommandLineArgs cmdlnArgs;
